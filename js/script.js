e2b5962b9b1e5520768f83ff07e36dbaf0e1c56d
angelomarcio40 <101677257+angelomarcio40@users.noreply.github.com>
2022-05-13 21:17:42 -0300

    Responsivo finalizado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+// Inicializa o Carousel
 const myCarousel = new Carousel(document.querySelector("#myCarousel"), {
     // Your options go here
   });
+
+  // Função que oculta e exibe o menu
+  const exibeMenu = () => {
+  // muda a visualição dos itmes com classe oculta-navbar, mudando de hide(oculto) para show(exibir)
+    $('.oculta-navbar').toggle()
+  }
+  // Final da função que oclutan e exibe o menu
